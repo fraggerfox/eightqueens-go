b10893823e491c44a074b233d6c4e76104ec1d2e
Santhosh Raju <santhosh.raju@gmail.com>
2023-03-04 12:17:33 +0100

    Complete the implementation of row, col and diagonal checks.

--- eightqueens/eightqueens.go
+++ eightqueens/eightqueens.go
@@ -5,12 +5,34 @@ const BOARD_SIZE = 8
 type Board [BOARD_SIZE][BOARD_SIZE]bool
 type Boards []Board
 
+// checkRowColumn returns true if it is safe to place the queen
+// in given position, false if a conflict is found.
 func checkRowColumn(board Board, row, col int) bool {
-	panic("not implemented")
+	for i := 0; i < BOARD_SIZE; i++ {
+		if board[row][i] || board[i][col] {
+			return false
+		}
+	}
+
+	return true
 }
 
+// checkDiagonal returns true if it is safe to place the queen
+// in given position, false if a conflict is found.
 func checkDiagonal(board Board, row, col int) bool {
-	panic("not implemented")
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if board[i][j] {
+			return false
+		}
+	}
+
+	for i, j := row-1, col+1; i >= 0 && j < BOARD_SIZE; i, j = i-1, j+1 {
+		if board[i][j] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func IsSafe(board Board, row, col int) bool {
--- eightqueens/eightqueens_internal_test.go
+++ eightqueens/eightqueens_internal_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// Reference for tests using algebraic notation
+// https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
+//
+// + (0)(1)(2)(3)(4)(5)(6)(7)
+// 8 |__|##|__|##|__|##|__|##| (0)
+// 7 |##|__|##|__|##|__|##|__| (1)
+// 6 |__|##|__|##|__|##|__|##| (2)
+// 5 |##|__|##|__|##|__|##|__| (3)
+// 4 |__|##|__|##|__|##|__|##| (4)
+// 3 |##|__|##|__|##|__|##|__| (5)
+// 2 |__|##|__|##|__|##|__|##| (6)
+// 1 |##|__|##|__|##|__|##|__| (7)
+// +  a  b  c  d  e  f  g  h
+//
+// For example board[0][0] is a8 and board[3][6] is g5 an so on.
 type EightQueensSuite struct {
 	suite.Suite
 	board Board
@@ -23,11 +38,95 @@ func (suite *EightQueensSuite) TestEmptyBoard() {
 }
 
 func (suite *EightQueensSuite) TestCheckRowColumn() {
-	assert.PanicsWithValue(suite.T(), "not implemented", func() { checkRowColumn(suite.board, 0, 0) })
+	tcs := []struct {
+		name           string
+		existingQueens map[int]int
+		row            int
+		column         int
+		want           bool
+	}{
+		{
+			name:           "Check for conflicts in an empty board, a2",
+			existingQueens: map[int]int{},
+			row:            6,
+			column:         0,
+			want:           true,
+		},
+		{
+			name:           "Existing queen in a8, should conflict with b8",
+			existingQueens: map[int]int{0: 0},
+			row:            0,
+			column:         1,
+			want:           false,
+		},
+		{
+			name:           "Existing queen in a8 and c7, should conflict with c1",
+			existingQueens: map[int]int{0: 0, 1: 2},
+			row:            7,
+			column:         2,
+			want:           false,
+		},
+		{
+			name:           "Existing queen in a8, c7 and e6, should be safe with d5",
+			existingQueens: map[int]int{0: 0, 1: 2, 2: 4},
+			row:            3,
+			column:         3,
+			want:           true,
+		},
+	}
+
+	for _, tc := range tcs {
+		for row, col := range tc.existingQueens {
+			suite.board[row][col] = true
+		}
+		assert.Equalf(suite.T(), tc.want, checkRowColumn(suite.board, tc.row, tc.column), "%s: expected: %v, got: %v", tc.name, tc.want, !tc.want)
+	}
 }
 
 func (suite *EightQueensSuite) TestCheckDiagonal() {
-	assert.PanicsWithValue(suite.T(), "not implemented", func() { checkDiagonal(suite.board, 0, 0) })
+	tcs := []struct {
+		name           string
+		existingQueens map[int]int
+		row            int
+		column         int
+		want           bool
+	}{
+		{
+			name:           "Check for conflicts in an empty board, a2",
+			existingQueens: map[int]int{},
+			row:            6,
+			column:         0,
+			want:           true,
+		},
+		{
+			name:           "Existing queen in a8, should conflict with h1",
+			existingQueens: map[int]int{0: 0},
+			row:            7,
+			column:         7,
+			want:           false,
+		},
+		{
+			name:           "Existing queen in a8 and c7, should conflict with f4",
+			existingQueens: map[int]int{0: 0, 1: 2},
+			row:            4,
+			column:         5,
+			want:           false,
+		},
+		{
+			name:           "Existing queen in a8, c7 and b6, should be safe with g5",
+			existingQueens: map[int]int{0: 0, 1: 2, 2: 1},
+			row:            3,
+			column:         6,
+			want:           true,
+		},
+	}
+
+	for _, tc := range tcs {
+		for row, col := range tc.existingQueens {
+			suite.board[row][col] = true
+		}
+		assert.Equalf(suite.T(), tc.want, checkDiagonal(suite.board, tc.row, tc.column), "%s: expected: %v, got: %v", tc.name, tc.want, !tc.want)
+	}
 }
 
 func (suite *EightQueensSuite) TestPlaceQueen() {
